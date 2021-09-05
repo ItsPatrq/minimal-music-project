035507296a1fbd87c0041394067a7a76527119ea
Patryk Bieszke <peinpat1@gmail.com>
2021-09-05 22:39:34 +0200

    Cookies detailed consents part1

--- _includes/cookies.js
+++ _includes/cookies.js
@@ -1,33 +1,61 @@
-(function() {
-    const consentPropName = "cookies_consent";
-    const storageType = localStorage;
 
-    areCookiesAccepted = () => storageType.getItem(consentPropName);
-    acceptCookieConsent = () => storageType.setItem(consentPropName, true);
+(function() {
+    const interactedWithCookiesBanner = "interacted_with_cookies_banner";
+    const storageType = localStorage;
+    const cookieContainerId = "cookieContainer";
+
+    acceptAllCookieConsent = () => {
+        {% for consent in site.data.metaData.cookies.consent %}
+            eval("storageType.setItem('{{consent.cookieName}}', true);");
+        {% endfor %}
+    }
+
+    window.cookiesAcceptDefaultFn = () => {
+        const cookieContainer = document.getElementById(cookieContainerId);
+        cookieContainer.classList.add("hidden");
+        window.setTimeout(cookieContainer.remove.bind(cookieContainer), 1000);
+        storageType.setItem(interactedWithCookiesBanner, true);
+        acceptAllCookieConsent();
+    }
+    window.cookiesDisagreeDefaultFn = () => {
+        const cookieContainer = document.getElementById(cookieContainerId);
+        cookieContainer.classList.add("hidden");
+        window.setTimeout(cookieContainer.remove.bind(cookieContainer), 1000);
+        storageType.setItem(interactedWithCookiesBanner, true);
+        window.location = "cookies";
+    }
+    
     window.addEventListener("load", () => {
-        if(areCookiesAccepted()) {
+        if(storageType.getItem(interactedWithCookiesBanner) === "true" || window.location.href.indexOf("cookies") !== -1) {
             return;
         }
         const cookieContainer = document.createElement("div");
+        cookieContainer.setAttribute("id", cookieContainerId);
         cookieContainer.classList.add("hidden");
+
+        const cookieHeader = document.createElement("h2");
+        const cookieHeaderContent = document.createTextNode("{{ site.data.metaData.cookies.header }}");
+
         const cookieParagraph = document.createElement("p");
-        const cookieContent = document.createTextNode("{{ site.data.metaData.cookies.message }}");
+        const cookieParagraphContent = document.createTextNode("{{ site.data.metaData.cookies.message }}");
+
+        const cookieButtonsContainer = document.createElement("div");
+        cookieButtonsContainer.classList.add("cookie-container--buttons");
+
         const cookieAgreeButton = document.createElement("button");
         const cookieButtonContent = document.createTextNode("{{ site.data.metaData.cookies.agreeButtonText }}");
         cookieAgreeButton.appendChild(cookieButtonContent);
-        cookieAgreeButton.onclick = () => {
-            acceptCookieConsent();
-            cookieContainer.classList.add("hidden");
-            window.setTimeout(cookieContainer.remove.bind(cookieContainer), 1000);
-            window.setTimeout(cookieContainer.remove.bind(cookieContainer), 1000);
-            try {
-                {{ site.data.metaData.cookies.agreeButtonFnName }}();
-            } catch(e) {
-                console.error(e);
-            }
-        }
-        cookieParagraph.append(cookieContent);
-        cookieContainer.append(cookieParagraph, cookieAgreeButton);
+        eval("cookieAgreeButton.onclick = {{ site.data.metaData.cookies.agreeButtonFnName }};");
+
+        const cookieButtonDisagree = document.createElement("button");
+        const cookieButtonDisagreeContent = document.createTextNode("{{ site.data.metaData.cookies.disagreeButtonText }}");
+        cookieButtonDisagree.append(cookieButtonDisagreeContent);
+        eval("cookieButtonDisagree.onclick = {{ site.data.metaData.cookies.disagreeButtonFnName }};");
+
+        cookieHeader.append(cookieHeaderContent);
+        cookieParagraph.append(cookieParagraphContent);
+        cookieButtonsContainer.append(cookieAgreeButton, cookieButtonDisagree)
+        cookieContainer.append(cookieHeader, cookieParagraph, cookieButtonsContainer);
         cookieContainer.classList.add("cookie-container")
         document.body.appendChild(cookieContainer);
         window.setTimeout(() => cookieContainer.classList.remove("hidden"));
