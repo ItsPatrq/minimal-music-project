4250749659a1bf05830afe8d4f1324ee25b20bf8
Patryk Bieszke <peinpat1@gmail.com>
2021-09-09 23:00:16 +0200

    Added zoomable photos

--- _includes/common.js
+++ _includes/common.js
@@ -2,4 +2,26 @@
     window.toggleExpander = function toggleExpander(x) {
         document.getElementsByClassName("menu")[0].classList.toggle("expanded");
     }
+    window.addEventListener("load", () => {
+        const images = document.querySelectorAll("img[click-to-enlarge]");
+        images.forEach(img => {
+            img.addEventListener("click", () => {
+                const src = img.attributes.getNamedItem("src");
+                console.log(src)
+                const modal = document.createElement("div");
+                const removeModal = function() {
+                    document.body.removeChild(modal);
+                };
+                modal.classList.add("modal__picture-zoomed");
+                modal.style.backgroundImage = `URL("${src.value}")`;
+                modal.addEventListener("click", removeModal);
+                document.body.append(modal);
+                document.body.addEventListener("keyup", e => {
+                    if (e.key === 'Escape') {
+                        removeModal();
+                    }
+                }, {once : true})
+            });
+        });
+    })
 })();
